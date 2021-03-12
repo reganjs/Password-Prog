25ea5ab532ec1a4fd2464d6fa9d9a2d54d59ef29
Regan <52940978+reganjs@users.noreply.github.com>
2021-03-11 21:32:28 -0600

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,24 @@
+# Regan Setter
+# Student ID 3505894
+# Program 3 - Loops And If Conditions
+# February 4th, 2021
+
+# A program written in Python that requests a password from the user and then asks for their name. Dependant
+# on what name is input, a different result may show
+
+password = 'Hello'
+name = 'Barney'
+
+while True:
+    p: str = input('Password? ')
+    if p == password:
+        break
+
+print('Welcome to the second half of the program!')
+n = input('What is your name? ')
+if n in ['Madonna', 'Cher']:
+    print('May I have your autograph, please?')
+elif n == name:
+    print('What a great name!')
+else:
+    print('{0}, that’s a nice name.'.format(n))
